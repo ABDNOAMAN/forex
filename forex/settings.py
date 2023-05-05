892fa21ee4068d118c88e1baad5db130e28085b4
ABDNOAMAN <114725229+ABDNOAMAN@users.noreply.github.com>
2023-05-05 15:21:58 +0300

    Update settings.py

--- forex/settings.py
+++ forex/settings.py
@@ -4,6 +4,20 @@ USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15
 SPIDER_MODULES = ["forex.spiders"]
 NEWSPIDER_MODULE = "forex.spiders"
 
+# Add Your ScrapeOps API Key
+SCRAPEOPS_API_KEY = 'e4ea08af-ef35-4354-885d-e75b34979a52'
+
+# Add In The ScrapeOps Extension
+EXTENSIONS = {
+        'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
+        }
+
+# Update The Download Middlewares
+DOWNLOADER_MIDDLEWARES = {
+'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550,
+'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+}
+
 DOWNLOAD_HANDLERS = {
     "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
